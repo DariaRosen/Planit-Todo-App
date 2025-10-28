1cd25bbdcc162fe543b5fd3b555c7ae1b2f1bf84
Daria Rosen <daria.sk135@gmail.com>
2025-10-28 17:00:13 +0200

    Add Tasks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { PlanitLayout } from "./layouts/PlanitLayout"
 import { WeekPlanner } from "./pages/WeekPlanner"
+import { Tasks } from "./pages/Tasks"
 
 export function App() {
   return (
@@ -8,6 +9,7 @@ export function App() {
       <Routes>
         <Route element={<PlanitLayout />}>
           <Route path="/" element={<WeekPlanner />} />
+          <Route path="/tasks" element={<Tasks />} />
         </Route>
       </Routes>
     </Router>
