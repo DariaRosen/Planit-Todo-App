fa4f459ed9562a7677221309b447aef067867d64
Daria Rosen <daria.sk135@gmail.com>
2025-10-31 14:25:03 +0200

    Add UserPanel ti App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { PlanitLayout } from "./layouts/PlanitLayout"
 import { WeekPlanner } from "./pages/WeekPlanner"
 import { Tasks } from "./pages/Tasks"
+import { UserPanel } from "./components/UserPanel"
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
         <Route element={<PlanitLayout />}>
           <Route path="/" element={<WeekPlanner />} />
           <Route path="/tasks" element={<Tasks />} />
+          <Route path="/user" element={<UserPanel />} />
         </Route>
       </Routes>
     </Router>
